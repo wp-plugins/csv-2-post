6866f984f4390524e78391858851abb7dc8a6b0a
WebTechGlobal <WebTechGlobal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-02 01:09:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/csv-2-post/trunk@188820 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- layouts_csv2post.php
+++
@@ -1,4 +0,0 @@
-<h2>CSV 2 POST Layout &amp; Styling</h2>
-<p>Use this page to edit "Custom Post Layouts" and configure how your posts will look</p>
-
-<h3>Coming Soon! Currently Under Construction</h3>
