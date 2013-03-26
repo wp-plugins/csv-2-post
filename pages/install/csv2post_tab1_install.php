204fec75cf802f93325528a50931389a3c7f3b41
WebTechGlobal <WebTechGlobal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-26 00:17:48 +0000

    Wide file structure change, new version following this purge
    git-svn-id: https://plugins.svn.wordpress.org/csv-2-post/trunk@687278 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pages/install/csv2post_tab1_install.php
+++
@@ -1,4 +0,0 @@
-<?php 
-global $csv2post_logcats_array;
-csv2post_log_display_bytype('install',100,$csv2post_logcats_array['categories']['install']['columns']);
-?>
--- script/multiselect/i18n/jquery.multiselect.filter.zh-cn.js
+++
@@ -1,7 +0,0 @@
-/* Simplified Chinese initialisation for the jQuery UI multiselect plugin. */
-/* Written by Ben (ben@zfben.com). */
-
-$.extend($.ech.multiselectfilter.prototype.options, {
-	label: '过滤:',
-	placeholder: '输入关键字过滤'
-});
