aff3dc853cb7d2d462c9db12ca39f4dbf9c8df61
WebTechGlobal <WebTechGlobal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-10 14:18:32 +0000

    Version 6.9.6 Released
    git-svn-id: https://plugins.svn.wordpress.org/csv-2-post/trunk@710942 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wtg-core/php/csv2post_php_gui.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Outputs email link, just a quick way of adding the link to various parts of the gui
+*/
+function csv2post_request_upgrade(){
+    echo '<a href="info@csv2post.com">Request Upgrade</a>';
+}
+?>
